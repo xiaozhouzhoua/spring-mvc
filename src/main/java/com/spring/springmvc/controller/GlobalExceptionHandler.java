b1e1be7ae746e41e0bd51c2dcffb4ae5efaf37f4
zhouguangping <1445101933@qq.com>
2022-02-25 22:35:25 +0800

    🦄错误返回优化

--- src/main/java/com/spring/springmvc/controller/GlobalExceptionHandler.java
+++ src/main/java/com/spring/springmvc/controller/GlobalExceptionHandler.java
@@ -15,13 +15,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Object handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
         BindingResult result = ex.getBindingResult();
-        String stringified = String.join("|",
-                result.getFieldErrors()
-                        .stream()
-                        .map(error -> error.getField() + ": " + error.getDefaultMessage())
-                        .collect(Collectors.toList()));
+        String stringField = result.getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining("|"));
         return ResponseEntity
                 .status(HttpStatus.PRECONDITION_FAILED)
-                .body(stringified);
+                .body(stringField);
     }
 }
